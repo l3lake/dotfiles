1ba5a66a3660463e0ba0cd1bef12589ec809c557
Blake <l3lake@users.noreply.github.com>
2014-11-04 20:38:22 -0800

    Update 50_osx.sh

--- source/50_osx.sh
+++ source/50_osx.sh
@@ -14,6 +14,11 @@ alias c="tr -d '\n' | pbcopy"
 # Start ScreenSaver. This will lock the screen if locking is enabled.
 alias ss="open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app"
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###############################################################################
 # From https://github.com/mathiasbynens/dotfiles/blob/master/.osx             #
