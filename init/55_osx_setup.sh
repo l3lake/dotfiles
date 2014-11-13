695789f1c5d30c372d576857f108c81e73c15ff2
Blake Anderson <blakekanderson@gmail.com>
2014-11-12 21:23:59 -0800

    uncommented disable boot startup chime

--- init/55_osx_setup.sh
+++ init/55_osx_setup.sh
@@ -25,7 +25,7 @@ sudo defaults write /Library/Preferences/.GlobalPreferences AppleInterfaceTheme 
 sudo pmset -a standbydelay 86400
 
 # Disable the sound effects on boot
-# sudo nvram SystemAudioVolume=" "
+sudo nvram SystemAudioVolume=%80
 
 # Disable transparency in the menu bar and elsewhere on Yosemite
 # defaults write com.apple.universalaccess reduceTransparency -bool true
