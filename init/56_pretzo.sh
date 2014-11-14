5890358de46b069e5dcc4a322bc1c812485461fe
Blake <l3lake@users.noreply.github.com>
2014-11-13 19:15:48 -0800

    Create 56_pretzo.sh

---
+++ init/56_pretzo.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+ln -s ~/.zprezto/runcoms/zlogin ~/.zlogin
+ln -s ~/.zprezto/runcoms/zlogout ~/.zlogout
+ln -s ~/.zprezto/runcoms/zpreztorc ~/.zpreztorc
+ln -s ~/.zprezto/runcoms/zprofile ~/.zprofile
+ln -s ~/.zprezto/runcoms/zshenv ~/.zshenv
+ln -s ~/.zprezto/runcoms/zshrc ~/.zshrc
