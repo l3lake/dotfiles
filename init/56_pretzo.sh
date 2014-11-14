9155091fa8500a3d49b30ec5e91d07e593af5b8d
Blake <l3lake@users.noreply.github.com>
2014-11-14 07:02:48 -0800

    Update 56_pretzo.sh

--- init/56_pretzo.sh
+++ init/56_pretzo.sh
@@ -1,13 +1,12 @@
-# OSX-only stuff. Abort if not OSX.
-#is_osx || return 1
-
+# Clone Prezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
+# Link configuration files
 ln -s ~/.zprezto/runcoms/zlogin ~/.zlogin
 ln -s ~/.zprezto/runcoms/zlogout ~/.zlogout
-#ln -s ~/.zprezto/runcoms/zpreztorc ~/.zpreztorc
 ln -s ~/.zprezto/runcoms/zprofile ~/.zprofile
 ln -s ~/.zprezto/runcoms/zshenv ~/.zshenv
 ln -s ~/.zprezto/runcoms/zshrc ~/.zshrc
 
+# Make zsh default shell
 chsh -s /bin/zsh
